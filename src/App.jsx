2cd09c4c012665635bb4e8aff3fe692ef0d6a2f2
MatthewAndresWestMec <111930814+MatthewAndresWestMec@users.noreply.github.com>
2024-01-09 14:35:45 -0700

    comments

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import DynamicList from './Components/Dynamic-List-Component';
 import data from './data'
 function App() {
   return (
+    // prop drilled data and type settings. change type to 'ul' / 'ol' for unordered list or ordered list
    <DynamicList data={data} type={'ol'}/>
   );
 }
